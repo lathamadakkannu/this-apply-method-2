efdbde9d1ee9f11f0d267d27b6f154fce0ed31a5
lathamadakkannu <lathamadakkannu@gmail.com>
2023-11-05 18:53:25 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,23 @@
+const computer = {
+    name: 'MacBook',
+    isOn: false,
+    turnOn() {
+        this.isOn = true;
+        return `The ${this.name} is On`;
+    },
+    turnOff() {
+        this.isOn = false;
+        return `The ${this.name} is Off`;
+    }
+};
+
+const server = {
+    name: 'Dell PowerEdge T30',
+    isOn: false
+};
+
+
+const result=computer.turnOn.apply(server)
+
+console.log(result);
+
